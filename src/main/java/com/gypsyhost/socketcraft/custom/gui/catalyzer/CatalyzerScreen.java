88275cb99800fa189dff7deab4f922d8f41fd2db
SuurflieG <suurflieg@rocketmail.com>
2022-04-17 15:06:40 +0200

    Changes to screen

--- src/main/java/com/gypsyhost/socketcraft/custom/gui/catalyzer/CatalyzerScreen.java
+++ src/main/java/com/gypsyhost/socketcraft/custom/gui/catalyzer/CatalyzerScreen.java
@@ -12,25 +12,37 @@ import net.minecraft.world.entity.player.Inventory;
 public class CatalyzerScreen extends AbstractContainerScreen<CatalyzerMenu> {
         private static final ResourceLocation TEXTURE = new ResourceLocation(SocketCraft.MOD_ID, "textures/gui/catalyzer_gui.png");
 
+        int imageHeight = 188;
+        int imageWidth = 176;
+        int titleLabelX = 8;
+        int titleLabelY = 6;
+        int inventoryLabelX = 8;
+        int inventoryLabelY = this.imageHeight - 96;
+
         public CatalyzerScreen(CatalyzerMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
         }
 
-
-
         @Override
         protected void init() {
                 super.init();
-                }
-        
+        }
+
+        @Override
+        protected void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
+                this.font.draw(pPoseStack, this.title, (float)this.titleLabelX, (float)this.titleLabelY, 4210752);
+                this.font.draw(pPoseStack, this.playerInventoryTitle, (float)this.inventoryLabelX, (float)this.inventoryLabelY, 4210752);
+        }
+
         @Override
         protected void renderBg(PoseStack pPoseStack, float pPartialTicks, int pMouseX, int pMouseY) {
                 RenderSystem.setShader(GameRenderer::getPositionTexShader);
                 RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
                 RenderSystem.setShaderTexture(0, TEXTURE);
-                int imageHeight = 187;
-                int x = (width - imageWidth) / 2;
-                int y = (height - imageHeight) / 2;
+
+
+                int x = (width / 2 )  - (imageWidth / 2);/* find center of screen */ /*move image over by half its width */
+                int y = (height / 2 ) - (imageHeight / 2);
         
                 this.blit(pPoseStack, x, y, 0, 0, imageWidth, imageHeight);
         
