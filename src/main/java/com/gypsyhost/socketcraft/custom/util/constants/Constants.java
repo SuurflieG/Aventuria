3cbf98a67221b72aaaf433ba12f330bff87bc596
SuurflieG <suurflieg@rocketmail.com>
2022-04-28 23:06:43 +0200

    Adding some Constants for future stuff

---
+++ src/main/java/com/gypsyhost/socketcraft/custom/util/constants/Constants.java
@@ -0,0 +1,22 @@
+package com.gypsyhost.socketcraft.custom.util.constants;
+
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.BooleanSupplier;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    // region MOD IDS
+    public static final String ID_MINECRAFT = "minecraft";
+    public static final String ID_FORGE = "forge";
+
+    public static final String ID_SOCKETCRAFT_CORE = "socketcraft_core";
+
+    public static final String ID_SOCKETCRAFT = "socketcraft";
+    // end region
+}
